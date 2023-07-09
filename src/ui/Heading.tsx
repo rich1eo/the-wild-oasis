8f0f0da35f7970a405faaff2ef66efec04e90df1
Vladislav Rychkov <v.richkov20@gmail.com>
2023-07-09 14:42:46 +0300

    Create Layout, Supabase integration for remote state

---
+++ src/ui/Heading.tsx
@@ -0,0 +1,25 @@
+import { css, styled } from 'styled-components';
+
+const Heading = styled.h1<{ as: 'h1' | 'h2' | 'h3' }>`
+  ${props =>
+    props.as === 'h1' &&
+    css`
+      font-size: 3rem;
+      font-weight: 700;
+    `}
+  ${props =>
+    props.as === 'h2' &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+  ${props =>
+    props.as === 'h3' &&
+    css`
+      font-size: 2rem;
+      font-weight: 500;
+    `}
+  line-height: 1.4;
+`;
+
+export default Heading;
---
+++ src/ui/Sidebar.tsx
@@ -0,0 +1,22 @@
+import { styled } from 'styled-components';
+import Logo from './Logo';
+import MainNav from './MainNav';
+
+const StyledSidebar = styled.aside`
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+  grid-row: 1 / -1;
+  background-color: var(--color-grey-0);
+  padding: 3.4rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+`;
+
+export default function Sidebar() {
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
+}
